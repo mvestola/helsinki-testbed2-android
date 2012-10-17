41dc01e2f954095e43f4e52a85364909b725c69d
Mikko Vestola <mikko.vestola@iki.fi>
2012-10-18 00:06:45 +0300

    Added a simple working robotium test.

--- robotium/src/main/java/fi/testbed2/robotium/MainActivityRobotiumTest.java
+++ robotium/src/main/java/fi/testbed2/robotium/MainActivityRobotiumTest.java
@@ -2,48 +2,45 @@ package fi.testbed2.robotium;
 
 import android.test.ActivityInstrumentationTestCase2;
 import com.jayway.android.robotium.solo.Solo;
+import fi.testbed2.R;
 import fi.testbed2.activity.MainActivity;
 import junit.framework.Assert;
 
 /**
- * This is a simple framework for a test of an Application.  See
- * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
- * how to write and extend Application tests.
- * <p/>
- * To run this test, you can type:
- * adb shell am instrument -w \
- * -e class fi.testbed2.activity.MainActivityTest \
- * fi.testbed2.tests/android.test.InstrumentationTestRunner
+ * Robotium test for the main activity.
  */
 public class MainActivityRobotiumTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     private Solo solo;
 
     public MainActivityRobotiumTest() {
-        super("fi.testbed2", MainActivity.class);
+        super(MainActivity.class);
     }
 
-    public void testPreferenceIsSaved() throws Exception {
-
-        solo.sendKey(Solo.MENU);
-        solo.clickOnText("More");
-        solo.clickOnText("Preferences");
-        solo.clickOnText("Edit File Extensions");
-        Assert.assertTrue(solo.searchText("rtf"));
-
-        solo.clickOnText("txt");
-        solo.clearEditText(2);
-        solo.enterText(2, "robotium");
-        solo.clickOnButton("Save");
-        solo.goBack();
-        solo.clickOnText("Edit File Extensions");
-        Assert.assertTrue(solo.searchText("application/robotium"));
-
+    @Override
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
     }
 
     @Override
     public void tearDown() throws Exception {
         solo.finishOpenedActivities();
+    }
+
+    public void testPreferenceIsSaved() throws Exception {
+
+        solo.sendKey(Solo.MENU);
+        solo.clickOnText(getLocalizedString(R.string.main_menu_preferences));
+        solo.clickOnText(getLocalizedString(R.string.preference_map_type_title));
+        solo.clickOnText("Kosteus");
+        solo.goBack();
+
+        Assert.assertTrue(solo.searchText("Helsinki Testbed Viewer"));
+
+    }
+
+    private String getLocalizedString(int id) {
+        return getActivity().getResources().getString(id);
     }
 
 }
